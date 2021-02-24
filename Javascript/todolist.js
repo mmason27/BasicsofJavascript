812795c60cb31231e2b990ca0d7a366174b0f757
mmason27 <mmmason27@gmail.com>
2021-02-24 09:47:43 -0500

    fixed remove button so it works on both lists

--- Javascript/todolist.js
+++ Javascript/todolist.js
@@ -5,36 +5,33 @@ let completedTaskList = document.getElementById("completedTaskList");
 let buttonSubmit = document.getElementById("buttonSubmit");
 
 buttonSubmit.addEventListener("click", function() {
-    let taskName = addTaskBox.value;
+  
+   //creating the checkbox to move task from pending task list to completed task list
+    let checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.addEventListener("click", function() {
+        if (this.checked) {
+            completedTaskList.appendChild(listItem);
+        }
+        else {
+            pendingTaskList.appendChild(listItem);
+        }
+    })
 
+    // //creating the list item
+    let taskName = addTaskBox.value;
     let listItem = document.createElement("li");
     listItem.innerHTML = taskName;
 
     //creating the remove button
     let removeButton = document.createElement("button");
     removeButton.innerHTML = "Remove";
-    removeButton.addEventListener("click", function(list) {
-        if (list == pendingTaskList) { //if item is in pending task list, delete it from there
-            pendingTaskList.removeChild(this.parentElement);
-        }
-        else {
-            completedTaskList.removeChild(this.parentElement);
-        }
-    })
-    
-    //creating the checkbox to move task from pending task list to completed task list
-    let checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.addEventListener("click", function() {
-        if (checkbox.checked == true) {
-            completedTaskList.appendChild(listItem);
-        }
-        else if (checkbox.checked == false) {
-            pendingTaskList.appendChild(listItem);
-        }
+    removeButton.addEventListener("click", function() {
+        this.parentElement.remove();
     })
 
     listItem.appendChild(checkbox);
-    pendingTaskList.appendChild(listItem);
     listItem.appendChild(removeButton);
+    pendingTaskList.appendChild(listItem);
+    
 })
